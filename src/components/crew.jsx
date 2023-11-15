6b6a057a5695dcf09686c178b883cb346473be14
vigas <vigas54209@gmail.com>
2023-11-15 14:51:19 +0530

    added all files

---
+++ src/components/crew.jsx
@@ -0,0 +1,54 @@
+import React, {useState} from "react";
+import Nav  from "./nav";
+import data from "../data.json";
+import bgImg from "../assets/crew/background-crew-desktop.jpg";
+
+function Crew(){
+    const [value, setvalue] = useState(0);
+    const crewName = ["Douglas Hurley","Mark Shuttleworth","Victor Glover","Anousheh Ansari"]; 
+
+    // Findind the selected destination from the data
+    const selectedCrew = data.crew.find(
+      (crew) => crew.name.toLowerCase() === crewName[value].toLowerCase()
+    );
+    
+    return(
+
+        <>
+            <div style={{backgroundImage: `url(${bgImg})`, backgroundSize: 'cover', minHeight: '100vh' }}>
+                <Nav />
+                <div className="crewCont">
+                    <div className="leftAreaCrew">
+                    
+                        <div className="crewNameCont">
+                            <h1 className="crewDesg" style={{opacity: 0.5}}>{selectedCrew.role}</h1>
+                            <h1 className="crewName">{selectedCrew.name}</h1>
+                            <p className="crewBio">{selectedCrew.bio}</p>
+                            <div className="buttonColl">
+                                <button className="button" onClick={()=>{
+                                    setvalue(0)
+                                }}></button>
+                                <button className="button" onClick={()=>{
+                                    setvalue(1)
+                                }}></button>
+                                <button className="button" onClick={()=>{
+                                    setvalue(2)
+                                }}></button>
+                                <button className="button" onClick={()=>{
+                                    setvalue(3)
+                                }}></button>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="rightAreaCrew">
+                        <img src={selectedCrew.images.webp} alt={selectedCrew.name} />
+                    </div>
+                </div>
+
+            </div>   
+        </>    
+    );
+}
+
+export default Crew;
---
+++ src/components/nav.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../assets/shared/logo.svg'
+
+
+function Nav() {
+
+  return (
+    <div className='navBar'>
+
+        <img className='logoImg' src={logo} alt='logo'/>
+        <hr className='navHr'></hr>
+        <ul className='navs'>
+            <li> <Link to="/">00  HOME</Link> </li>
+            <li> <Link to='/destination'>01 DESTINATION</Link> </li>
+            <li> <Link to='/crew'>02 CREW</Link> </li>
+            <li> <Link to='/technology'>03 TECHNOLOGY</Link> </li>
+        </ul> 
+        
+    </div>
+  );
+}
+
+export default Nav;
